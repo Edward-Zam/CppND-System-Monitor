31848578fa284e3204ce75aa517a0997a8675bc7
Edward Zamora <edward.zamora@outlook.com>
2023-08-24 05:55:16 +0000

    Added experimental Pids function to use std::filesystem.

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -22,6 +22,7 @@ const std::string kPasswordPath{"/etc/passwd"};
 float MemoryUtilization();
 long UpTime();
 std::vector<int> Pids();
+std::vector<int> Pids2();
 int TotalProcesses();
 int RunningProcesses();
 std::string OperatingSystem();
--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <experimental/filesystem>
+#include <iostream>
 
 #include "linux_parser.h"
 
@@ -52,11 +54,13 @@ vector<int> LinuxParser::Pids() {
   vector<int> pids;
   DIR* directory = opendir(kProcDirectory.c_str());
   struct dirent* file;
+  std::cerr << "PIDS:" << std::endl;
   while ((file = readdir(directory)) != nullptr) {
     // Is this a directory?
     if (file->d_type == DT_DIR) {
       // Is every character of the name a digit?
       string filename(file->d_name);
+      std::cerr << filename << std::endl;
       if (std::all_of(filename.begin(), filename.end(), isdigit)) {
         int pid = stoi(filename);
         pids.push_back(pid);
@@ -64,6 +68,24 @@ vector<int> LinuxParser::Pids() {
     }
   }
   closedir(directory);
+  return pids;
+}
+
+vector<int> LinuxParser::Pids2(){
+  vector<int>pids;
+  //auto it = std::experimental::filesystem::directory_iterator("/proc/");
+  for(const auto& entry : std::experimental::filesystem::directory_iterator("/proc/"))
+  {
+    if (std::experimental::filesystem::is_directory(entry.status()))
+    {
+      string filename(entry.path().string());
+      if (std::all_of(filename.begin(), filename.end(), isdigit)) {
+        int pid = stoi(filename);
+        pids.push_back(pid);
+      }
+    }
+  }
+  //closedir(directory);
   return pids;
 }
 
