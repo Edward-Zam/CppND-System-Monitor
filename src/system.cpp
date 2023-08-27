be10284e0d25be3d1282bbdbbbe8f0636c66436b
Edward Zamora <edward.zamora@outlook.com>
2023-08-27 03:02:08 +0000

    Filled most methods for the system class.

--- src/system.cpp
+++ src/system.cpp
@@ -17,7 +17,7 @@ using std::vector;
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a container composed of the system's processes
+// Return a container composed of the system's processes
 vector<Process>& System::Processes() {
   vector<int> pids = LinuxParser::Pids();
   for( auto i:pids )
@@ -28,23 +28,36 @@ vector<Process>& System::Processes() {
   // We've overloaded the < operator, don't need to 
   // pass in a sort method
   std::sort(processes_.begin(), processes_.end());
+
   return processes_;
 }
 
-// TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+// Return the system's kernel identifier (string)
+std::string System::Kernel() {
+  return LinuxParser::Kernel();
+}
 
-// TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+// Return the system's memory utilization
+float System::MemoryUtilization() {
+  return LinuxParser::MemoryUtilization();
+}
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// Return the operating system name
+std::string System::OperatingSystem() {
+  return LinuxParser::OperatingSystem();
+}
 
-// TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+// Return the number of processes actively running on the system
+int System::RunningProcesses() {
+  return LinuxParser::RunningProcesses();
+}
 
-// TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+// Return the total number of processes on the system
+int System::TotalProcesses() {
+  return LinuxParser::TotalProcesses();
+}
 
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// Return the number of seconds since the system started running
+long int System::UpTime() {
+  return LinuxParser::UpTime() / sysconf(_SC_CLK_TCK);
+}
