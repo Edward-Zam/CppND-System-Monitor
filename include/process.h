b78dc67b77c0d38acec7f2e8cc7a8fa552b6420e
Edward Zamora <edward.zamora@outlook.com>
2023-08-27 02:20:53 +0000

    Cleanup and filled in more functions to process class.

--- include/process.h
+++ include/process.h
@@ -10,11 +10,11 @@ class Process {
  public:
   Process(int pid);
   int Pid() const;
-  std::string User();                      
-  std::string Command();                   
+  std::string User() const;                      
+  std::string Command() const;                   
   float CpuUtilization() const;                  
-  std::string Ram();                       
-  long int UpTime();                       
+  std::string Ram() const;                       
+  long int UpTime() const;                       
   bool operator<(Process const& a) const;  
 
   // TODO: Declare any necessary private members
--- src/process.cpp
+++ src/process.cpp
@@ -14,7 +14,7 @@ using std::vector;
 // Constructor
 Process::Process(int pid)
 {
-  // Grab that value that won't change here
+  // Grab that values that won't change here
   _iPid = pid;
   _sUid = LinuxParser::Uid(_iPid);
   _sUser = LinuxParser::User(_iPid);
@@ -31,32 +31,35 @@ float Process::CpuUtilization() const {
   long upTime = LinuxParser::UpTime(_iPid);
   if( upTime > 0 )
   {
-    return float(LinuxParser::ActiveJiffies(_iPid_)) / float(upTime);
+    return float(LinuxParser::ActiveJiffies(_iPid)) / float(upTime);
   }
   return 0;
 }
 
-// TODO: Return the command that generated this process
+// Return the command that generated this process
 string Process::Command() const {
   return _sCommand;
 }
 
-// TODO: Return this process's memory utilization
+// Return this process's memory utilization
 string Process::Ram() const {
   long megaBytes = stol(LinuxParser::Ram(_iPid)) / 1000;
   return to_string(megaBytes);
 }
 
-// TODO: Return the user (name) that generated this process
+// Return the user (name) that generated this process
 string Process::User() const {
   return _sUser;
 }
 
-// TODO: Return the age of this process (in seconds)
+// Return the age of this process (in seconds)
 long int Process::UpTime() const {
-  return stol(LinuxParser::UpTime(_iPid) / sysconf(_SC_CLK_TCK));
+  return LinuxParser::UpTime(_iPid) / sysconf(_SC_CLK_TCK);
 }
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+// Overload the "less than" comparison operator for Process objects
+bool Process::operator<(Process const& a) const { 
+  // We can use CPU Utilization or RAM to sort
+  // return Ram() < a.Ram();
+  return CpuUtilization() < a.CpuUtilization();
+}
